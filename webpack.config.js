21264ef77bcb280cf161cfd89ae543ca250b7eb6
witaliy-D <witaliy999@gmail.com>
2019-12-01 00:23:14 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
           loader: 'babel-loader',
-          query: {
-            presets: ['@babel/preset-env']
-          }
-        }
       }
     ]
   },
